dc97ce6a671a8e2cc3cf57015b4763318c38fab1
Roberto Costa <roberto.araujo339@gmail.com>
2023-06-29 02:24:30 -0300

    go back navigation

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { FlatList } from 'react-native';
-
+import { useNavigation } from '@react-navigation/native';
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
@@ -11,6 +11,12 @@ import { Container } from './styles';
 
 export function Groups() {
 	const [groups, setGroups] = useState<string[]>([]);
+
+	const navigation = useNavigation();
+
+	function handleNewGroup() {
+		navigation.navigate('new');
+	}
 
 	return (
 		<Container>
@@ -27,7 +33,7 @@ export function Groups() {
 				)}
 			/>
 
-			<Button title="Criar nova turma" />
+			<Button title="Criar nova turma" onPress={handleNewGroup} />
 		</Container>
 	);
 }
