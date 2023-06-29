41719ac290dfbc048fd12a4c1090d5eff8707865
Roberto Costa <roberto.araujo339@gmail.com>
2023-06-29 00:24:59 -0300

    ListEmpty component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,30 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+
 import { Container } from './styles';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
+	const [groups, setGroups] = useState<string[]>([]);
+
 	return (
 		<Container>
 			<Header />
 			<Highlight title="Turmas" subtitle="jogue com sua turma" />
 
-			<GroupCard title="Galera do Ignite" />
+			<FlatList
+				data={groups}
+				keyExtractor={(item) => item}
+				renderItem={({ item }) => <GroupCard title={item} />}
+				contentContainerStyle={groups.length === 0 && { flex: 1 }}
+				ListEmptyComponent={() => (
+					<ListEmpty message="Que tal cadastrar a primeira turma?" />
+				)}
+			/>
 		</Container>
 	);
 }
